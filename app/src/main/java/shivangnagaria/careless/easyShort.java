a2ce626a608a0499fb7ad49c4b4b6fb604f73bb1
shivtej1505 <15electronicmotor@gmail.com>
2015-08-10 23:26:02 +0530

    first run completed

---
+++ app/src/main/java/shivangnagaria/careless/easyShort.java
@@ -0,0 +1,11 @@
+package shivangnagaria.careless;
+
+public class easyShort {
+
+    public class prefs {
+        public final static String HINT_QUES = "hintQuestion";
+        public final static String HINT_ANS = "hintAnswer";
+        public final static String PASS = "userPassword";
+        public final static String FIRST_RUN_DONE = "firstRunDone";
+    }
+}
--- app/src/main/java/shivangnagaria/careless/firstRun.java
+++ app/src/main/java/shivangnagaria/careless/firstRun.java
@@ -1,16 +1,84 @@
 package shivangnagaria.careless;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class firstRun extends ActionBarActivity {
+public class firstRun extends Activity {
 
+    SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+    @Override
+    protected void onStart() {
+
+        if(preferences.getBoolean(easyShort.prefs.FIRST_RUN_DONE,false)) {
+            Intent jmp = new Intent(firstRun.this,appEntry.class);
+            startActivity(jmp);
+            return;
+        }
+        super.onStart();
+    }
+
+    String pickQ,ansE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // creating reference
         setContentView(R.layout.activity_first_run);
+        final EditText pass = (EditText) findViewById(R.id.pass);
+        final EditText passAgain = (EditText) findViewById(R.id.passAgain);
+        Spinner spinQ = (Spinner) findViewById(R.id.spinQ);
+        final EditText ansQ = (EditText) findViewById(R.id.ansQ);
+        Button saveBtn = (Button) findViewById(R.id.saveBtn);
+
+        // setting adapter to spinner
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(firstRun.this,R.array.hintQ,R.layout.support_simple_spinner_dropdown_item);
+        spinQ.setAdapter(arrayAdapter);
+        spinQ.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                pickQ = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(firstRun.this,"Please select a question",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ansE = ansQ.getText().toString();
+                if(!(pass.getText().toString().equals(passAgain.getText().toString()))) {
+                    if(pass.getText().toString().length() < 8 || passAgain.getText().toString().length() < 8 ) {
+                        Toast.makeText(firstRun.this, "Minimum password length 8", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(firstRun.this,"Password didn't match",Toast.LENGTH_SHORT).show();
+                    }
+                } else if(ansE.equals("")) {
+                    Toast.makeText(firstRun.this,"Please provide answer",Toast.LENGTH_SHORT).show();
+                } else {
+                    // save details in SharedPreferences
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString(easyShort.prefs.HINT_QUES,pickQ);
+                    editor.putString(easyShort.prefs.HINT_ANS,ansE);
+                    editor.putString(easyShort.prefs.PASS,pass.getText().toString());
+                    editor.putBoolean(easyShort.prefs.FIRST_RUN_DONE,true);
+                    editor.commit();
+                }
+            }
+        });
     }
 
     @Override
